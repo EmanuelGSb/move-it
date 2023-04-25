a4f3cf0fe77a7663e4b6341a12b66206129dbf41
Emanuel <emanuelgsbr@gmail.com>
2023-04-24 22:00:38 -0300

    returnXpDefault

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,12 +1,14 @@
-export function ExperinceBar (){
+export function ExperinceBar() {
   return (
     <header className="experince-bar">
       <span>0 xp</span>
       <div>
-        <div style={{ width: '50%' }} />
-        <span className="current-experience" style={{ left: '50%' }}>300 xp</span>
+        <div style={{ width: "50%" }} />
+        <span className="current-experience" style={{ left: "50%" }}>
+          300 xp
+        </span>
       </div>
-      <span>601 xp</span>
+      <span>600 xp</span>
     </header>
   )
 }
